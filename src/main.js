4d25cfe713cf43d5563e26849b9e664352f70ae9
Doan Duy Dat <doanduydat98@gmail.com>
2020-10-18 21:56:43 +0700

    add validation

--- src/main.js
+++ src/main.js
@@ -5,12 +5,15 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import VueSweetalert2 from 'vue-sweetalert2';
+import { ValidationProvider } from 'vee-validate';
+import "./plugins/validation";
 
 // If you don't need the styles, do not connect
 import 'sweetalert2/dist/sweetalert2.min.css';
 
 Vue.use(VueSweetalert2);
 Vue.config.productionTip = false;
+Vue.component('ValidationProvider', ValidationProvider);
 
 new Vue({
   router,
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,23 +1,42 @@
 <template>
   <v-main>
     <v-container class="d-flex flex-column justify-center screen--full">
-      <v-card width="50%" class="pa-10 mx-auto">
+      <v-card
+        :width="$vuetify.breakpoint.xs ? '100%' : '50%'"
+        class="pa-10 mx-auto"
+      >
         <div class="text-h5 text-center mb-5">
           Đăng nhập
         </div>
         <v-form>
-          <v-text-field
-            v-model="form.email"
-            label="Email"
-            type="email"
-            outlined
-          ></v-text-field>
-          <v-text-field
-            v-model="form.password"
-            label="Password"
-            type="password"
-            outlined
-          ></v-text-field>
+          <ValidationProvider
+            mode="lazy"
+            name="email"
+            rules="required|email"
+            v-slot="{ errors }"
+          >
+            <v-text-field
+              v-model="form.email"
+              label="Email"
+              type="email"
+              outlined
+            ></v-text-field>
+            <span class="red--text text--lighten-1">{{ errors[0] }}</span>
+          </ValidationProvider>
+          <ValidationProvider
+            mode="lazy"
+            name="password"
+            rules="required|alpha_dash|min:6"
+            v-slot="{ errors }"
+          >
+            <v-text-field
+              v-model="form.password"
+              label="Password"
+              type="password"
+              outlined
+            ></v-text-field>
+            <span class="red--text text--lighten-1">{{ errors[0] }}</span>
+          </ValidationProvider>
         </v-form>
         <v-card-actions class="d-flex justify-end">
           <v-btn color="primary" @click="login">Đăng nhập</v-btn>
