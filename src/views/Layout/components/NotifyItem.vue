5494669f6e251e8fe199d51909d061b307a2ce19
Doan Duy Dat <doanduydat98@gmail.com>
2021-01-18 23:32:02 +0700

    update link notify

--- src/views/Layout/components/NotifyItem.vue
+++ src/views/Layout/components/NotifyItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-list-item link>
+  <v-list-item link :href="getLink()">
     <v-list-item-avatar>
       <v-icon :color="icon.color" large>
         {{ icon.text }}
@@ -17,6 +17,8 @@
 </template>
 
 <script>
+import authService from "@/api/authentication";
+
 export default {
   props: {
     data: {
@@ -38,6 +40,21 @@ export default {
     shortDate: timeStamp => {
       let d = new Date(timeStamp);
       return d.toLocaleDateString();
+    },
+    getLink() {
+      const role = authService.getRole();
+      const notify = this.data;
+      const baseUrl = "http://localhost:8080";
+      if (notify.type === "newCv") {
+        if (role === "volunteer") {
+          return `${baseUrl}/volunteerHome/view-cv/${notify.cvId}`;
+        }
+        if (role === "specialist") {
+          return `${baseUrl}/specialistHome/view-cv/${notify.cvId}`;
+        }
+      } else if (notify.type === "newReview") {
+        return `${baseUrl}/memberHome/review-result/${notify.reviewId}`;
+      }
     }
   }
 };
